310cacbc555cd68980e00c59bf0d61140a990470
El Morjani Mohamed <elmorjanimohamed9@gmail.com>
2024-04-15 18:49:49 +0100

    add PropertyAmenity controller

---
+++ app/Http/Controllers/PropertyAmenityController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PropertyAmenity;
+use App\Http\Requests\StorePropertyAmenityRequest;
+use App\Http\Requests\UpdatePropertyAmenityRequest;
+
+class PropertyAmenityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePropertyAmenityRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PropertyAmenity $propertyAmenity)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PropertyAmenity $propertyAmenity)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePropertyAmenityRequest $request, PropertyAmenity $propertyAmenity)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PropertyAmenity $propertyAmenity)
+    {
+        //
+    }
+}
