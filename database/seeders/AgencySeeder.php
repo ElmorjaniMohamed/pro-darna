09c5dcf03ce4d98570173f9fb5303e6a8729366a
El Morjani Mohamed <elmorjanimohamed9@gmail.com>
2024-04-17 13:21:39 +0100

    feat: Add initial data seeding for agencies

--- database/seeders/AgencySeeder.php
+++ database/seeders/AgencySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Agency;
 
 class AgencySeeder extends Seeder
 {
@@ -12,6 +13,22 @@ class AgencySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $agencies = [
+            [
+                'name' => 'Agency 1',
+                'description' => 'Description for Agency 1',
+                'address' => '123 Street, City, State, Country',
+                'zipCode' => 12345,
+                'phone' => '123-456-7890',
+                'email' => 'agency1@example.com',
+                'number_of_agent' => 10,
+                'webSite' => 'http://www.agency1.com',
+            ],
+            // Add more agencies as needed...
+        ];
+
+        foreach ($agencies as $agency) {
+            Agency::create($agency);
+        }
     }
 }
