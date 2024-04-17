eb7d3cf7659c217f1d3dd4ecc60936dc7d458c8b
El Morjani Mohamed <elmorjanimohamed9@gmail.com>
2024-04-17 14:11:01 +0100

    Add property amenities to PropertyAmenitySeeder

--- database/seeders/PropertyAmenitySeeder.php
+++ database/seeders/PropertyAmenitySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\PropertyAmenity; 
 
 class PropertyAmenitySeeder extends Seeder
 {
@@ -12,6 +13,23 @@ class PropertyAmenitySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $amenities = [
+            'Air Conditioning',
+            'Bedding',
+            'Balcony',
+            'Cable TV',
+            'Oven',
+            'Internet',
+            'Parking',
+            'Lift',
+            'Pool',
+            'Dishwasher',
+            'Washing Machine',
+            'Toaster',
+        ];
+
+        foreach ($amenities as $amenity) {
+            PropertyAmenity::create(['name' => $amenity]);
+        }
     }
 }
