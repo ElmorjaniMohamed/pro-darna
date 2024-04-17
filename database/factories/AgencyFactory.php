65be48f6769f41166367cfd6167b997d02e40d20
El Morjani Mohamed <elmorjanimohamed9@gmail.com>
2024-04-17 14:10:30 +0100

    Add fields to AgencyFactory

--- database/factories/AgencyFactory.php
+++ database/factories/AgencyFactory.php
@@ -3,12 +3,20 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Agency;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Agency>
  */
 class AgencyFactory extends Factory
 {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Agency::class;
+    
     /**
      * Define the model's default state.
      *
@@ -17,7 +25,14 @@ class AgencyFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->company,
+            'description' => $this->faker->paragraph,
+            'address' => $this->faker->address,
+            'zipCode' => substr($this->faker->postcode, 0, 5),
+            'phone' => $this->faker->phoneNumber,
+            'email' => $this->faker->companyEmail,
+            'number_of_agent' => $this->faker->numberBetween(1, 50),
+            'webSite' => $this->faker->url,
         ];
     }
 }
