a29e60c7b320fa0ece7738c1e1b6135b2d8cf16c
El Morjani Mohamed <elmorjanimohamed9@gmail.com>
2024-04-17 14:10:50 +0100

    Refactor AgencySeeder to use factory method

--- database/seeders/AgencySeeder.php
+++ database/seeders/AgencySeeder.php
@@ -13,22 +13,6 @@ class AgencySeeder extends Seeder
      */
     public function run(): void
     {
-        $agencies = [
-            [
-                'name' => 'Agency 1',
-                'description' => 'Description for Agency 1',
-                'address' => '123 Street, City, State, Country',
-                'zipCode' => 12345,
-                'phone' => '123-456-7890',
-                'email' => 'agency1@example.com',
-                'number_of_agent' => 10,
-                'webSite' => 'http://www.agency1.com',
-            ],
-            // Add more agencies as needed...
-        ];
-
-        foreach ($agencies as $agency) {
-            Agency::create($agency);
-        }
+        Agency::factory()->count(10)->create();
     }
 }
