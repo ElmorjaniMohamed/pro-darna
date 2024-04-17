cdde48d725ddd61ac046f5b91695e3812100f8a3
El Morjani Mohamed <elmorjanimohamed9@gmail.com>
2024-04-17 15:58:18 +0100

    Add $fillable property to PropertyAmenity model

--- app/Models/PropertyAmenity.php
+++ app/Models/PropertyAmenity.php
@@ -9,6 +9,8 @@ class PropertyAmenity extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['name'];
+
     public function properties()
     {
         return $this->belongsToMany(Property::class, 'amenity_property');
