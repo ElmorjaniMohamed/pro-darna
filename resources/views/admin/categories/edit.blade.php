0a1a2c8bed841b37e6a30f16c31683d4ecca891c
El Morjani Mohamed <elmorjanimohamed9@gmail.com>
2024-04-21 14:25:43 +0100

    Refactor category edit form and remove starter page

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -17,8 +17,9 @@
                     <label for="title"
                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
                     <input type="text" name="name" id="name" value="{{$category->name}}"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                        class="form-input"
                         placeholder="Name" required="">
+                    <x-common.error-message-input name="name" />
                 </div>
             </div>
             <button type="submit" class="btn btn-primary !mt-6">
--- resources/views/admin/index.blade.php
+++
@@ -1,5 +0,0 @@
-<x-layout.default>
-    <div>
-        <h1>starter page</h1>
-    </div>
-</x-layout.default>
