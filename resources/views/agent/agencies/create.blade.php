5ea061145b341f9ecaba177e01b3d074ad25cd1d
El Morjani Mohamed <elmorjanimohamed9@gmail.com>
2024-04-21 14:25:24 +0100

    Refactor form inputs and error messages

--- resources/views/agent/agencies/create.blade.php
+++ resources/views/agent/agencies/create.blade.php
@@ -16,74 +16,56 @@
                         <label for="gridName">Name</label>
                         <input id="gridName" name="name" type="text" placeholder="Enter Name"
                             class="form-input" />
-                        @error('name')
-                            <div class="text-red-500">{{ $message }}</div>
-                        @enderror
+                        <x-common.error-message-input name="name" />
                     </div>
                     <div>
                         <label for="gridEmail">Email</label>
                         <input id="gridEmail" name="email" type="email" placeholder="Enter Email"
                             class="form-input" />
-                        @error('email')
-                            <div class="text-red-500">{{ $message }}</div>
-                        @enderror
+                        <x-common.error-message-input name="email" />
                     </div>
                 </div>
                 <div>
                     <label for="gridDescription">Description</label>
                     <textarea id="mde-autosave" name="description" class="form-input" placeholder="Enter Description"></textarea>
-                    @error('description')
-                        <div class="text-red-500">{{ $message }}</div>
-                    @enderror
+                    <x-common.error-message-input name="description" />
                 </div>
                 <div>
                     <label for="gridAddress">Address</label>
                     <input id="gridAddress" name="address" type="text" placeholder="Enter Address"
                         class="form-input" />
-                    @error('address')
-                        <div class="text-red-500">{{ $message }}</div>
-                    @enderror
+                    <x-common.error-message-input name="address" />
                 </div>
-                <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
                     <div>
                         <label for="gridPhone">Phone</label>
                         <input id="gridPhone" name="phone" type="text" placeholder="Enter Phone"
                             class="form-input" />
-                        @error('phone')
-                            <div class="text-red-500">{{ $message }}</div>
-                        @enderror
+                       <x-common.error-message-input name="phone" />
                     </div>
                     <div>
                         <label for="gridZipCode">Zip Code</label>
                         <input id="gridZipCode" name="zipCode" type="text" placeholder="Enter Zip Code"
                             class="form-input" />
-                        @error('zipCode')
-                            <div class="text-red-500">{{ $message }}</div>
-                        @enderror
+                        <x-common.error-message-input name="zipCode" />
                     </div>
                     <div>
                         <label for="gridNumberOfAgent">Number of Agents</label>
                         <input id="gridNumberOfAgent" name="number_of_agent" type="number"
                             placeholder="Enter Number of Agents" class="form-input" />
-                        @error('number_of_agent')
-                            <div class="text-red-500">{{ $message }}</div>
-                        @enderror
+                        <x-common.error-message-input name="number_of_agent" />
                     </div>
                     <div>
                         <label for="gridWebsite">Website</label>
                         <input id="gridWebsite" name="webSite" type="text" placeholder="Enter Website URL"
                             class="form-input" />
-                        @error('webSite')
-                            <div class="text-red-500">{{ $message }}</div>
-                        @enderror
+                        <x-common.error-message-input name="webSite" />
                     </div>
                 </div>
                 <div>
                     <label for="gridImage">Image</label>
                     <input id="gridImage" name="image" type="file" class="form-input py-0" />
-                    @error('image')
-                        <div class="text-red-500">{{ $message }}</div>
-                    @enderror
+                    <x-common.error-message-input name="image" />
                 </div>
                 <button type="submit" class="btn btn-primary !mt-6">Create</button>
             </form>
--- resources/views/agent/agencies/edit.blade.php
+++ resources/views/agent/agencies/edit.blade.php
@@ -8,82 +8,73 @@
                     Edit Agency</h3>
             </div>
             <!-- Modal body -->
-            <form class="space-y-5" method="POST" action="{{ route('agencies.update', $agency->id) }}" enctype="multipart/form-data">
+            <form class="space-y-5" method="POST" action="{{ route('agencies.update', $agency->id) }}"
+                enctype="multipart/form-data">
                 @csrf
                 @method('PUT')
 
                 <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
                     <div>
                         <label for="gridName">Name</label>
-                        <input id="gridName" name="name" type="text" value="{{ $agency->name }}" class="form-input" />
-                        @error('name')
-                            <div class="text-red-500">{{ $message }}</div>
-                        @enderror
+                        <input id="gridName" name="name" type="text" value="{{ $agency->name }}"
+                            class="form-input" />
+                        <x-common.error-message-input name="name" />
                     </div>
                     <div>
                         <label for="gridEmail">Email</label>
-                        <input id="gridEmail" name="email" type="email" value="{{ $agency->email }}" class="form-input" />
-                        @error('email')
-                            <div class="text-red-500">{{ $message }}</div>
-                        @enderror
+                        <input id="gridEmail" name="email" type="email" value="{{ $agency->email }}"
+                            class="form-input" />
+                        <x-common.error-message-input name="email" />
                     </div>
                 </div>
                 <div>
                     <label for="gridDescription">Description</label>
                     <textarea id="mde-autosave" name="description" class="form-input">{{ $agency->description }}</textarea>
-                    @error('description')
-                        <div class="text-red-500">{{ $message }}</div>
-                    @enderror
+                    <x-common.error-message-input name="description" />
                 </div>
                 <div>
                     <label for="gridAddress">Address</label>
-                    <input id="gridAddress" name="address" type="text" value="{{ $agency->address }}" class="form-input" />
-                    @error('address')
-                        <div class="text-red-500">{{ $message }}</div>
-                    @enderror
+                    <input id="gridAddress" name="address" type="text" value="{{ $agency->address }}"
+                        class="form-input" />
+                    <x-common.error-message-input name="address" />
                 </div>
-                <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
                     <div>
                         <label for="gridPhone">Phone</label>
-                        <input id="gridPhone" name="phone" type="text" value="{{ $agency->phone }}" class="form-input" />
-                        @error('phone')
-                            <div class="text-red-500">{{ $message }}</div>
-                        @enderror
+                        <input id="gridPhone" name="phone" type="text" value="{{ $agency->phone }}"
+                            class="form-input" />
+                        <x-common.error-message-input name="phone" />
                     </div>
                     <div>
                         <label for="gridZipCode">Zip Code</label>
-                        <input id="gridZipCode" name="zipCode" type="text" value="{{ $agency->zipCode }}" class="form-input" />
-                        @error('zipCode')
-                            <div class="text-red-500">{{ $message }}</div>
-                        @enderror
+                        <input id="gridZipCode" name="zipCode" type="text" value="{{ $agency->zipCode }}"
+                            class="form-input" />
+                        <x-common.error-message-input name="zipCode" />
                     </div>
                     <div>
                         <label for="gridNumberOfAgent">Number of Agents</label>
-                        <input id="gridNumberOfAgent" name="number_of_agent" type="number" value="{{ $agency->number_of_agent }}" class="form-input" />
-                        @error('number_of_agent')
-                            <div class="text-red-500">{{ $message }}</div>
-                        @enderror
+                        <input id="gridNumberOfAgent" name="number_of_agent" type="number"
+                            value="{{ $agency->number_of_agent }}" class="form-input" />
+                        <x-common.error-message-input name="number_of_agent" />
                     </div>
                     <div>
                         <label for="gridWebsite">Website</label>
-                        <input id="gridWebsite" name="webSite" type="text" value="{{ $agency->webSite }}" class="form-input" />
-                        @error('webSite')
-                            <div class="text-red-500">{{ $message }}</div>
-                        @enderror
+                        <input id="gridWebsite" name="webSite" type="text" value="{{ $agency->webSite }}"
+                            class="form-input" />
+                        <x-common.error-message-input name="webSite" />
                     </div>
                 </div>
                 <div>
                     <label for="gridImage">Image</label>
                     @if ($agency->media->isNotEmpty())
-                    <div class="px-4 py-4 flex justify-center items-center">
-                        <img src="{{ asset('storage/' . $agency->media->first()->file_path) }}" alt="Agency Image"
-                            class="w-96 h-40 object-cover rounded">
-                    </div>
                     @endif
                     <input id="gridImage" name="image" type="file" class="form-input py-0" />
-                    @error('image')
-                        <div class="text-red-500">{{ $message }}</div>
-                    @enderror
+
+                    <div class="px-2 py-4 flex justify-center items-center">
+                        <img src="{{ asset('storage/' . $agency->media->first()->file_path) }}" alt="Agency Image"
+                            class="w-80 h-44 rounded-md">
+                    </div>
+                    <x-common.error-message-input name="image" />
                 </div>
                 <button type="submit" class="btn btn-primary !mt-6">Update</button>
             </form>
