0624bf972acf9da9a58afa0ffd9fd1628817b9c5
El Morjani Mohamed <elmorjanimohamed9@gmail.com>
2024-04-21 17:49:28 +0100

    Add properties.blade.php file

--- resources/views/components/layout/app.blade.php
+++ resources/views/components/layout/app.blade.php
@@ -81,58 +81,65 @@
                                     </li>
                                 </ul>
                                 @if (Route::has('login') || Route::has('register'))
-                                @auth
-                                <ul class="flex flex-wrap items-center">
-                                    <li class="sm:mr-5 xl:mr-[20px] relative group"><a href="#">
-                                            <img src="assets/images/user/avater.png" loading="lazy" width="62"
-                                                height="62" alt="avater">
-                                        </a>
+                                    @auth
+                                        <ul class="flex flex-wrap items-center">
+                                            <li class="sm:mr-5 xl:mr-[20px] relative group"><a href="#">
+                                                    <img src="assets/images/user/avater.png" loading="lazy" width="62"
+                                                        height="62" alt="avater">
+                                                </a>
 
-                                        <ul
-                                            class="list-none bg-white drop-shadow-[0px_6px_10px_rgba(0,0,0,0.2)] rounded-[12px] flex flex-wrap flex-col w-[180px] absolute top-[120%] sm:left-1/2 sm:-translate-x-1/2 transition-all
+                                                <ul
+                                                    class="list-none bg-white drop-shadow-[0px_6px_10px_rgba(0,0,0,0.2)] rounded-[12px] flex flex-wrap flex-col w-[180px] absolute top-[120%] sm:left-1/2 sm:-translate-x-1/2 transition-all
                                             group-hover:top-[60px] invisible group-hover:visible opacity-0 group-hover:opacity-100 right-0">
-                                            <li
-                                                class="border-b border-dashed border-primary border-opacity-40 last:border-b-0 hover:border-solid transition-all">
-                                                <a href="login.html"
-                                                    class="font-lora leading-[1.571] text-[14px] text-primary p-[10px] capitalize block transition-all hover:bg-secondary hover:text-white text-center my-[-1px] rounded-t-[12px]">Profile</a>
-                                            </li>
+                                                    <li
+                                                        class="border-b border-dashed border-primary border-opacity-40 last:border-b-0 hover:border-solid transition-all">
+                                                        <a href="login.html"
+                                                            class="font-lora leading-[1.571] text-[14px] text-primary p-[10px] capitalize block transition-all hover:bg-secondary hover:text-white text-center my-[-1px] rounded-t-[12px]">Profile</a>
+                                                    </li>
 
-                                            <li
-                                                class="border-b border-dashed border-primary border-opacity-40 last:border-b-0 hover:border-solid transition-all">
-                                                <a href="register.html"
-                                                    class="font-lora leading-[1.571] text-[14px] text-primary p-[10px] capitalize block transition-all hover:bg-secondary hover:text-white text-center my-[-1px]">Dashboard</a>
+                                                    <li
+                                                        class="border-b border-dashed border-primary border-opacity-40 last:border-b-0 hover:border-solid transition-all">
+                                                        <a href="register.html"
+                                                            class="font-lora leading-[1.571] text-[14px] text-primary p-[10px] capitalize block transition-all hover:bg-secondary hover:text-white text-center my-[-1px]">Dashboard</a>
+                                                    </li>
+                                                    <li
+                                                        class="border-b border-dashed border-primary border-opacity-40 last:border-b-0 hover:border-solid transition-all">
+                                                        <form action="{{ route('logout') }}" method="POST">
+                                                            @csrf
+                                                            <button type="submit"
+                                                                class="font-lora w-full leading-[1.571] text-[14px] text-primary p-[10px] capitalize block transition-all hover:bg-secondary hover:text-white text-center my-[-1px] rounded-b-[12px]">
+                                                                Sign Out
+                                                            </button>
+                                                        </form>
+                                                    </li>
+
+                                                </ul>
                                             </li>
-                                            <li class="border-b border-dashed border-primary border-opacity-40 last:border-b-0 hover:border-solid transition-all">
-                                                <form action="{{ route('logout') }}" method="POST">
-                                                    @csrf
-                                                    <button type="submit" class="font-lora w-full leading-[1.571] text-[14px] text-primary p-[10px] capitalize block transition-all hover:bg-secondary hover:text-white text-center my-[-1px] rounded-b-[12px]">
-                                                        Sign Out
-                                                    </button>
-                                                </form>
+                                            @if(Auth::check() && Auth::user()->hasRole('agent'))
+                                                <li>
+                                                    <a href="{{ route('agencies.create') }}"
+                                                        class="sticky-btn before:rounded-md before:block before:absolute before:left-auto before:right-0 before:inset-y-0 before:-z-[1] before:bg-white before:w-0 hover:before:w-full hover:before:left-0 hover:before:right-auto hover:text-primary before:transition-all leading-none px-[20px] py-[15px] capitalize font-medium text-white hidden sm:block text-[14px] xl:text-[16px] relative after:block after:absolute after:inset-0 after:-z-[2] after:bg-secondary after:rounded-md after:transition-all">Add
+                                                        Agency</a>
+                                                </li>
+                                            @endif
+                                            <li class="ml-2 sm:ml-5 lg:hidden">
+                                                <a href="#offcanvas-mobile-menu"
+                                                    class="offcanvas-toggle flex text-[#016450] hover:text-secondary">
+                                                    <svg width="24" height="24" class="fill-current"
+                                                        xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                                        <path
+                                                            d="M0 96C0 78.33 14.33 64 32 64H416C433.7 64 448 78.33 448 96C448 113.7 433.7 128 416 128H32C14.33 128 0 113.7 0 96zM0 256C0 238.3 14.33 224 32 224H416C433.7 224 448 238.3 448 256C448 273.7 433.7 288 416 288H32C14.33 288 0 273.7 0 256zM416 448H32C14.33 448 0 433.7 0 416C0 398.3 14.33 384 32 384H416C433.7 384 448 398.3 448 416C448 433.7 433.7 448 416 448z" />
+                                                    </svg>
+                                                </a>
                                             </li>
-                                            
                                         </ul>
-                                    </li>
-                                    <li>
-                                        <a href="{{route('agencies.create')}}"
-                                            class="sticky-btn before:rounded-md before:block before:absolute before:left-auto before:right-0 before:inset-y-0 before:-z-[1] before:bg-white before:w-0 hover:before:w-full hover:before:left-0 hover:before:right-auto hover:text-primary before:transition-all leading-none px-[20px] py-[15px] capitalize font-medium text-white hidden sm:block text-[14px] xl:text-[16px] relative after:block after:absolute after:inset-0 after:-z-[2] after:bg-secondary after:rounded-md after:transition-all">Add
-                                            Agency</a>
-                                    </li>
-                                    <li class="ml-2 sm:ml-5 lg:hidden">
-                                        <a href="#offcanvas-mobile-menu"
-                                            class="offcanvas-toggle flex text-[#016450] hover:text-secondary">
-                                            <svg width="24" height="24" class="fill-current"
-                                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                                                <path
-                                                    d="M0 96C0 78.33 14.33 64 32 64H416C433.7 64 448 78.33 448 96C448 113.7 433.7 128 416 128H32C14.33 128 0 113.7 0 96zM0 256C0 238.3 14.33 224 32 224H416C433.7 224 448 238.3 448 256C448 273.7 433.7 288 416 288H32C14.33 288 0 273.7 0 256zM416 448H32C14.33 448 0 433.7 0 416C0 398.3 14.33 384 32 384H416C433.7 384 448 398.3 448 416C448 433.7 433.7 448 416 448z" />
-                                            </svg>
-                                        </a>
-                                    </li>
-                                </ul>
-                                @else
-                                <a href="{{ route('register') }}" class="sticky-btn before:rounded-md before:block before:absolute before:left-auto before:right-0 before:inset-y-0 before:-z-[1] before:bg-white before:w-0 hover:before:w-full hover:before:left-0 hover:before:right-auto hover:text-primary before:transition-all leading-none px-[20px] py-[15px] capitalize font-medium text-white hidden sm:block text-[14px] xl:text-[16px] relative after:block after:absolute after:inset-0 after:-z-[2] after:bg-secondary after:rounded-md after:transition-all">Get started</a>
-                                @endif
-                                @endauth
+                                    @else
+                                        <a href="{{ route('register') }}"
+                                            class="sticky-btn before:rounded-md before:block before:absolute before:left-auto before:right-0 before:inset-y-0 before:-z-[1] before:bg-white before:w-0 hover:before:w-full hover:before:left-0 hover:before:right-auto hover:text-primary before:transition-all leading-none px-[20px] py-[15px] capitalize font-medium text-white hidden sm:block text-[14px] xl:text-[16px] relative after:block after:absolute after:inset-0 after:-z-[2] after:bg-secondary after:rounded-md after:transition-all">Get
+                                            started</a>
+                                            @endauth
+                                    @endif
+                                
                             </nav>
                         </div>
                     </div>
