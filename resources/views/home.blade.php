0cdcedfe00f4185c346c9e9ebf2e9637e2c5ff71
El Morjani Mohamed <elmorjanimohamed9@gmail.com>
2024-04-22 14:24:37 +0100

    Remove newsletter section from home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5292,46 +5292,5 @@
     </section>
     <!-- Blog Section End  -->
 
-    <!-- News Letter section start -->
-    <section class="py-[80px] lg:p-[90px] bg-primary relative">
-        <div class="container">
-            <div class="grid grid-cols-1">
-                <div class="col-span">
-                    <div class="flex flex-wrap items-center justify-between">
-                        <div class="w-full lg:w-auto">
-                            <h3
-                                class="font-lora text-white text-[24px] sm:text-[30px] xl:text-[36px] leading-[1.277] mb-[10px]">
-                                Are you a Home Owner?</h3>
-                            <p class="text-secondary leading-[1.5] tracking-[0.03em] mb-10">Put your email address and get
-                                listed.</p>
-                            <form id="mc-form" action="#" class="relative w-full">
-                                <input id="mc-email"
-                                    class="font-light text-white leading-[1.75] opacity-100 border border-secondary w-full lg:w-[395px] xl:w-[495px] h-[60px] rounded-[10px] py-[15px] pl-[15px] pr-[15px] sm:pr-[135px] focus:border-white focus:outline-none border-opacity-60 placeholder:text-[#E2E2E2] bg-transparent"
-                                    type="text" placeholder="Enter your email here...">
-                                <button id="mc-submit" type="submit"
-                                    class="text-white font-medium text-[16px] leading-none tracking-[0.02em] bg-secondary py-[17px] px-[20px] mt-5 sm:mt-0 rounded-[10px] hover:bg-white hover:text-primary transition-all sm:absolute sm:right-[4px] sm:top-1/2 sm:-translate-y-1/2">Get
-                                    Listed</button>
-                            </form>
-                            <!-- mailchimp-alerts Start -->
-                            <div class="mailchimp-alerts text-centre">
-                                <div class="mailchimp-submitting"></div><!-- mailchimp-submitting end -->
-                                <div class="mailchimp-success text-green-400"></div><!-- mailchimp-success end -->
-                                <div class="mailchimp-error text-red-600"></div><!-- mailchimp-error end -->
-                            </div>
-                            <!-- mailchimp-alerts end -->
-                        </div>
-                        <div class="w-full hidden lg:block lg:w-auto mt-5 lg:mt-0">
-                            <div class="relative mt-10 md:mt-0 lg:absolute lg:right-0 lg:bottom-0">
-                                <img class="hero_image lg:max-w-[550px] xl:max-w-[650px] 2xl:max-w-[714px]"
-                                    src="assets/images/newsletter/bg-1.png" width="866" height="879"
-                                    alt="hero image">
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- News Letter section End -->
+    
 @endsection
