02ea78faf987f23d267aebc6915dcd3d8b97605f
El Morjani Mohamed <elmorjanimohamed9@gmail.com>
2024-04-22 19:26:05 +0100

    Add category support and sorting functionality to properties page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Property;
 use Illuminate\Http\Request;
 
@@ -15,9 +16,60 @@ class HomeController extends Controller
         return view('home', compact('properties'));
     }
 
-    public function properties()
+    public function properties(Request $request)
     {
-        $properties = Property::latest()->get();
-        return view('pages.properties', compact('properties'));
+        $sort = 'latest';
+        $properties = Property::latest()->paginate(6);
+        $categories = Category::all();
+
+        return view('pages.properties', compact('properties', 'sort', 'categories'));
     }
+
+    public function sort(Request $request)
+    {
+        $sort = $request->input('sort', 'latest');
+        $category = $request->input('category');
+
+        $properties = Property::query()
+            ->when($sort === 'latest', function ($query) {
+                return $query->latest();
+            })
+            ->when($sort === 'oldest', function ($query) {
+                return $query->oldest();
+            })
+            ->when($sort === 'price_high', function ($query) {
+                return $query->orderBy('price', 'desc');
+            })
+            ->when($sort === 'price_low', function ($query) {
+                return $query->orderBy('price', 'asc');
+            });
+
+        if ($category) {
+            $properties->where('category_id', $category);
+        }
+
+        $properties = $properties->paginate(6);
+
+        $categories = Category::all();
+
+        if ($request->ajax()) {
+            $view = view('pages.pagination', compact('properties', 'categories'))->render();
+            $pagination = $properties->appends(['sort' => $sort, 'category' => $category])->links('vendor.pagination.custom')->toHtml();
+
+            return response()->json(['view' => $view, 'pagination' => $pagination]);
+        }
+
+        return view('pages.properties', compact('properties', 'sort', 'categories'));
+    }
+
+    function propertiesDetails($id)
+    {
+        $property = Property::findOrFail($id);
+        $properties = Property::all();
+
+        return view('pages.properties-details', compact('property', 'properties'));
+    }
+
+
+
 }
