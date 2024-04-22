813d74f953bc7c7fef5394ea398e08114d834df6
El Morjani Mohamed <elmorjanimohamed9@gmail.com>
2024-04-22 19:26:53 +0100

    Add route for property details

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::view('/admin', 'admin.index');
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/sort-properties', [HomeController::class, 'sort']);
 Route::get('/properties', [HomeController::class, 'properties'])->name('properties');
+Route::get('/properties-details/{id}', [HomeController::class, 'propertiesDetails'])->name('properties-details');
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 Route::get('/verify', [VerifyController::class, 'verify'])->name('verification.notice');
 
