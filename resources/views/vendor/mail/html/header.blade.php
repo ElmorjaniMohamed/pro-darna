2297a6ae3cd949bd2c0022cca82e0f7645d468c7
El Morjani Mohamed <elmorjanimohamed9@gmail.com>
2024-04-26 12:27:17 +0100

    Update logo path in mail header and adjust button gradient colors

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ asset('assets/images/logo.svg') }}" class="logo" alt="Logo">
+<img src="{{ asset('assets/images/logo/logo.svg') }}" class="logo" alt="Logo">
 </a>
 </td>
 </tr>
